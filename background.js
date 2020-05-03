c7afccdaa979825a9fcb20032fed3a1d519cd907
agustin enriquez <agustin_enriquez@hotmail.com>
2020-05-02 22:49:20 -0300

    test img & notification message

--- background.js
+++ background.js
@@ -2,6 +2,17 @@ var slots = { 49: null, 50: null, 51: null, 52: null,53: null};
 var lastModifiedSlot = null;
 var flag = true;
 
+function createAlert(msg) {
+    chrome.notifications.create("addUrl", {   
+        type: "basic", 
+        title: "Slotr", 
+        message: msg,
+        iconUrl: "plus-flat.png"
+        }, function() {
+        console.log(chrome.runtime.lastError);
+    });
+}
+
 chrome.runtime.onConnect.addListener(function(port){
     console.assert(port.name == "slotr");
     if (flag) {
@@ -14,14 +25,7 @@ chrome.runtime.onConnect.addListener(function(port){
             port.postMessage({slots: slots});
             lastModifiedSlot = message.position;
             chrome.storage.sync.set({"slots": slots});
-            chrome.notifications.create("addUrl", {   
-                type: "basic", 
-                title: "Slotr", 
-                message: "saved url",
-                iconUrl: "plus-flat.png"
-                }, function() {
-                console.log(chrome.runtime.lastError);
-            });
+            createAlert("saved url");
         }
         if (message.openAll) {
             port.postMessage({openAll: message.openAll, openSlots: slots});
@@ -30,16 +34,19 @@ chrome.runtime.onConnect.addListener(function(port){
             slots[lastModifiedSlot] = null;
             chrome.storage.sync.set({"slots": slots});
             port.postMessage({slots: slots});
+            createAlert("last empty slot");
         }
         if (message.clearAll) {
             slots = {49: null, 50: null, 51: null, 52: null, 53: null};
             chrome.storage.sync.set({"slots": slots});
             port.postMessage({slots: slots});
+            createAlert("slots are now empty");
         }
         if (message.clearSlot) {
             slots[message.clearSlot] = null;
             chrome.storage.sync.set({"slots": slots});
             port.postMessage({slots: slots});
+            createAlert("selected slot is now empty");
         }
         if (message.showSlots) {
             chrome.storage.sync.set({"slots": slots});
